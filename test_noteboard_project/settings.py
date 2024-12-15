7c751bff8e26285a9733a0858c76e5e41ea2535b
kaiyukun <kaiyu.s0116@gmail.com>
2024-12-15 17:59:33 +0900

    メールアドレスを隠す

--- test_noteboard_project/settings.py
+++ test_noteboard_project/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kaiyu.s0116@gmail.com'
-EMAIL_HOST_PASSWORD = 'qzcotictadnobypk'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
